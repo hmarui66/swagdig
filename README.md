af9af36a245f7c72c66ac99ccefa732271ea231b
hmarui66 <hmarui66@gmail.com>
2020-08-14 10:44:50 +0900

    allow json format

--- README.md
+++ README.md
@@ -5,14 +5,24 @@ Path extractor of swagger.yml by a key name of paramters or definitions model
 ## How to use
 
 ```bash
-cat swagger.yml | pipenv run python swagdig.py -q foo_id
+$ curl -s https://petstore.swagger.io/v2/swagger.json | pipenv run python swagdig.py -q "category|quantity" -i json
+# => models:  ['Pet', 'Order']
+# => post	/pet
+# => put	/pet
+# => post	/store/order
 ```
 
 ## Command options
 
 ```
+usage: swagdig.py [-h] [-i INPUT] [-f FILEPATH] [-q QUERY] [-e EXCLUDE]
+
+swagdig.
+
 optional arguments:
   -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        input file type.(yaml|json). default yaml.
   -f FILEPATH, --filepath FILEPATH
                         yaml file path
   -q QUERY, --query QUERY
--- swagdig.py
+++ swagdig.py
@@ -2,17 +2,30 @@ import os
 import sys
 import argparse
 import yaml
+import json
 from digger import Digger
 
 
-def load_yaml(filepath):
-    if filepath == None or filepath == '':
-        return yaml.load(sys.stdin, Loader=yaml.SafeLoader)
+def is_specified(path):
+    return path != None and path != ''
+
+
+def load_yaml(file_type, filepath):
+    file = sys.stdin
+    if is_specified(filepath):
+        file = open(filepath)
 
     try:
-        with open(filepath) as file:
-            return yaml.safe_load(file)
+        res = None
+        if input == 'json':
+            res = json.load(file)
+        else:
+            res = yaml.safe_load(file)
 
+        if is_specified(filepath):
+            file.close()
+
+        return res
     except Exception as e:
         print('Exception occurred while loading YAML...', file=sys.stderr)
         print(e, file=sys.stderr)
@@ -20,7 +33,9 @@ def load_yaml(filepath):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='argparse sample.')
+    parser = argparse.ArgumentParser(description='swagdig.')
+    parser.add_argument('-i', '--input', type=str,
+                        help='input file type.(yaml|json). default yaml.')
     parser.add_argument('-f', '--filepath', type=str, help='yaml file path')
     parser.add_argument('-q', '--query', type=str,
                         help='query string(regex can be used)')
@@ -32,7 +47,7 @@ if __name__ == '__main__':
         print('query argument required', file=sys.stderr)
         exit(1)
 
-    obj = load_yaml(args.filepath)
+    obj = load_yaml(args.input, args.filepath)
 
     digger = Digger(args)
 
